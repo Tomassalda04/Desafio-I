f94f03f88681255bc87d2f60c5d62a93b18d3c37
Cristopher Corrales <cristopher.corrales@udea.edu.co>
2024-09-13 18:34:54 -0500

    Signed-off-by: Cristopher Corrales <cristopher.corrales@udea.edu.co>

--- Main.ino
+++ Main.ino
@@ -9,6 +9,14 @@ int estadoDetener = LOW;
 long tiempoLecturaIniciar = 0;
 long tiempoLecturaDetener = 0;
 long tiempoDelay = 50;  // Tiempo de debounce
+int capacidad=1;
+int numdatos=0;
+int *datos= new int [capacidad];
+int tamano;
+int amplitud;
+bool amplitudCalculada = false;
+
+
 
 int analogReadAverage(int pin, int samples) { 
   long sum = 0;
@@ -24,7 +32,24 @@ void setup() {
   Serial.begin(9600);  // Iniciar el monitor serial
 }
 
-void funcionamientoBotones() {
+void recodatos(int *&datos, int dato, int posicion, int capacidad){
+  datos[posicion]=dato;
+  //posicion+=1;
+  if(posicion==capacidad){
+  	int nuevacap= capacidad+1;
+    int *copydatos= new int[nuevacap];
+    for(int i=0; i<capacidad;i++){
+      copydatos[i]= datos[i];
+    }
+  delete[] datos;
+  datos=copydatos;
+  capacidad=nuevacap;
+  posicion+=1;
+  }
+}
+
+
+int* funcionamientoBotones() {
   int lecturaIniciar = digitalRead(botonIniciarPin);
   int lecturaDetener = digitalRead(botonDetenerPin);
 
@@ -39,6 +64,7 @@ void funcionamientoBotones() {
 
       if (estadoIniciar == HIGH) {
         recibiendo = true;  // Iniciar la recolección de datos
+       
         Serial.println("Recolección de datos iniciada...");
         delay(50);  // Pequeño delay para evitar rebotes
       }
@@ -58,25 +84,71 @@ void funcionamientoBotones() {
       if (estadoDetener == HIGH) {
         recibiendo = false;  // Detener la recolección de datos
         Serial.println("Recoleccion de datos detenida...");
+        for(int i=0;i<capacidad-1;i+=1){
+          Serial.print(datos[i]);
+          Serial.print(" ");
+        }
+        delete[] datos;
         delay(50);  // Pequeño delay para evitar rebotes
       }
     }
   }
+  
   ultimoEstadoDetener = lecturaDetener;
 
   // Si recibiendo es true, leer e imprimir la señal del generador
   if (recibiendo) {
     float valorSenal = analogRead(analogPin);  // Leer el valor de la señal
-    Serial.println(valorSenal);  // Imprimir el valor de la señal en el monitor serial
+    //datos = recodatos(datos,valorSenal,numdatos,capacidad);
+    //Serial.println(valorSenal); // Imprimir el valor de la señal en el monitor serial
+    datos[numdatos]=valorSenal;
+    numdatos+=1;
+    if(numdatos==capacidad){
+      int nuevacap= capacidad+1;
+      int *copydatos= new int[nuevacap];
+      for(int i=0; i<capacidad;i++){
+        copydatos[i]= datos[i];
+      }
+      delete[] datos;
+      datos=copydatos;
+      capacidad=nuevacap;
+    }
+    
     delay(100);  // Ajustar la frecuencia de lectura según sea necesario
   }
+  return datos;
 }
+
+int funAmplitud(int* arr, int tamano) {
+  int mayor = arr[0];
+  int menor = arr[0];
+
+  // Recorremos el arreglo para encontrar el mayor y menor
+  for (int i = 0; i < tamano; i++) {
+    if (arr[i] > mayor) {
+      mayor = arr[i];
+    }
+    if (arr[i] < menor) {
+      menor = arr[i];
+    }
+  }
+  // Retorna la suma de mayor y menor dividida entre 2
+  return ((mayor-menor)/2)/100;
+}
+
 
 void loop() {
-  funcionamientoBotones();  // Llamar a la función para manejar los botones y la recolección de datos
+  datos = funcionamientoBotones();// Llamar a la función para manejar los botones y la recolección de dato
+  
+  //PARA CALCULAR LA AMPLITUD
+  if (!amplitudCalculada && !recibiendo && numdatos > 0) {
+    tamano = numdatos; // Ajustar el tamaño con el número actual de datos
+    amplitud = funAmplitud(datos, tamano);
+    Serial.println();
+    Serial.print("La amplitud calculada es: ");
+    Serial.print(amplitud);
+    Serial.print(" V");
+    amplitudCalculada = true;
+  }
 }
-
-
-
-
 
